38df435bb413a3e97d7089aeb4cd2a24a7a99537
c-wrenn <crislynwren@yahoo.com>
2023-11-25 18:16:02 -0600

    "license functions moved from my old repo"

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const readmequestions = [
     message:"Choose a liscense for your project.",
     choices: ['GNU AGPLv3', 'GNU GPLv3', 
     'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License',
-    'Boost Software License 1.0', 'The Unlicense'], 
+    'Boost Software License 1.0', 'The Unlicense', 'none'], 
     name: 'liscense'
 
 },
